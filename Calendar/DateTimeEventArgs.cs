a3eeff153f333f8ac656ac5495af458d9464f2e0
João Pereira <jrpereira@gmail.com>
2020-01-16 11:37:06 +0000

    Updated to Xamarin Forms 4.4.0.991477. Code refactoring and cleaning

---
+++ Calendar/DateTimeEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ByteNuts.Xamarin.Controls.Calendar
+{
+	public class DateTimeEventArgs : EventArgs
+	{
+		public DateTime DateTime { get; set; }
+	}
+}
---
+++ Calendar/DateTypeEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace ByteNuts.Xamarin.Controls.Calendar
+{
+	public enum DateTypeEnum
+	{
+		Normal = 0,
+		Month = 1,
+		Year = 2
+	}
+}
